4af79924f53af2e453efd824f8c0439a098a3c43
sudharsfinale <sudharsfinale@gmail.com>
2025-05-06 20:38:18 +0530

    Comparison operators

--- app.py
+++ app.py
@@ -154,3 +154,21 @@ print(bool("Hello"))  # Output: True
 print(bool([1, 2, 3]))  # Output: True
 print(bool({1, 2, 3}))  # Output: True
 print(bool((1, 2, 3)))  # Output: True
+
+# Comparison Operators ~ used to compare values
+# Equal to
+print(10 == 10)  # Output: True
+print(10 == 20)  # Output: False
+print(10 == "10")  # Output: False (Because of type)
+# Not equal to
+print(10 != 10)  # Output: False
+print(10 != 20)  # Output: True
+# Greater than
+print(10 > 20)  # Output: False
+print(20 > 10)   # Output: True
+# Less than
+print(10 < 20)  # Output: True
+print(20 < 10)   # Output: False
+# Greater than or equal to
+print(10 >= 20)  # Output: False
+print(20 >= 10)  # Output: True
