508d1248b7224705bd3d8acb4bbed8d2ccc1cb25
sudharsfinale <sudharsfinale@gmail.com>
2025-05-10 12:40:32 +0530

    Working with pandas

---
+++ pandas_new.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# to work with one dimensional entry
+series_1 = pd.Series([1.5, 2.5, 3, 4.5, 5.0, 6])
+print(series_1)
+
+"""
+Output : 
+0    1.5
+1    2.5
+2    3.0
+3    4.5
+4    5.0
+5    6.0
+dtype: float64
+"""
